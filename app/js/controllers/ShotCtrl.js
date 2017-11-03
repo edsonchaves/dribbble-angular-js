054e26079b063b36127f38f3d201680bf1a25d51
thiagofuzaro <fuzarothiago@gmail.com>
2017-11-03 12:37:22 -0200

    Add files via upload

---
+++ app/js/controllers/ShotCtrl.js
@@ -0,0 +1,58 @@
+(function() {
+    'use strict';
+
+    angular
+        .module( 'App' )
+        .controller( 'ShotCtrl', [ '$scope', 'ServiceURL', 'XHRFactory', '$routeParams', 'AlertMessages', 'URLParams', function( $scope, ServiceURL, XHRFactory, $routeParams, AlertMessages, URLParams ) {
+            // variáveis expostas
+            $scope.shotId = $routeParams.id;
+            $scope.shot = [];
+            $scope.serviceFail = "";
+            $scope.messages = AlertMessages;
+            
+            // funções expostas
+            $scope.shotLike = shotLike;
+
+            function getCurrentShot() {
+                var getCurrentShotURL = ServiceURL.getCurrentShotURI
+                .replace( '{id}', $scope.shotId )
+                .replace( '{accessToken}', URLParams.accessToken + localStorage.clientAccessToken );
+
+                XHRFactory
+                .get( getCurrentShotURL )
+                .then ( getCurrentShotDone, getCurrentShotFail );
+            }
+
+            function getCurrentShotDone( response ) {
+                $scope.shot = response.data;
+            }
+
+            function getCurrentShotFail( response ) {
+                $scope.serviceFail = $scope.messages.currentShotFail;
+            }
+
+            function shotLike() {
+                var shotLikeURL = ServiceURL.getLikeURI
+                .replace( '{id}', $scope.shotId )
+                .replace( '{accessToken}', URLParams.accessToken + localStorage.clientAccessToken );
+
+                XHRFactory
+                .post( shotLikeURL )
+                .then ( shotLikeDone, shotLikeFail );
+            }
+
+            function shotLikeDone( response ) {
+                console.log( response );
+            }
+
+            function shotLikeFail( response ) {
+                $scope.serviceFail = $scope.messages.likeShotFail;
+            }
+
+            function init() {
+                getCurrentShot();
+            }
+
+            init();
+        }]);
+}());
